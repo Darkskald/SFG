c30cf828de66a9017b9c3c3b2ed1e0cfcb0da3cc
Darkskald <32447347+Darkskald@users.noreply.github.com>
2019-08-09 17:08:54 +0200

    - bugfix of sample states2

--- orm.py
+++ orm.py
@@ -13,268 +13,282 @@ from spectrum import SfgSpectrum, SfgAverager, LtIsotherm
 from sqlalchemy import create_engine, Column, Integer, Text, ForeignKey, UniqueConstraint, TIMESTAMP, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 import numpy as np
 
 import re
 import datetime
 
+Base = declarative_base()
+
+
+
+class SFG(Base):
+    __tablename__ = "sfg"
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    measured_time = Column(TIMESTAMP)
+    measurer = Column(Text)
+    type = Column(Text)
+    wavenumbers = Column(Text)
+    sfg = Column(Text)
+    ir = Column(Text)
+    vis = Column(Text)
+
+    __table_args__ = (UniqueConstraint("name", "type"),)
+
+
+class BoknisEck(Base):
+    __tablename__ = 'boknis_eck'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, unique=True)
+    specid = Column(Integer, ForeignKey('sfg.id'))
+
+
+class GasExSfg(Base):
+    __tablename__ = 'gasex_sfg'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, ForeignKey('sfg.name'), unique=True)
+    sample_id = Column(Integer, ForeignKey('samples.id'))
+    sample_hash = Column(Text)
+
+
+class RegularSfg(Base):
+    __tablename__ = 'regular_sfg'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    specid = Column(Integer, ForeignKey('sfg.id'))
+    surfactant = Column(Text)
+    surfactant_vol = Column(Text)
+    surfactant_conc = Column(Text)
+    sensitizer = Column(Text)
+    sensitizer_vol = Column(Text)
+    sensitizer_conc = Column(Text)
+    photolysis = Column(Text)
+    sample_no = Column(Text)
+    measurement_no = Column(Text)
+    comment = Column(Text)
+
+
+class Lt(Base):
+    __tablename__ = 'lt'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    type = Column(Text)
+    measured_time = Column(TIMESTAMP)
+    measurer = Column(Text)
+    time = Column(Text)
+    area = Column(Text)
+    apm = Column(Text)
+    surface_pressure = Column(Text)
+    lift_off = Column(Text)
+
+    __table_args__ = (UniqueConstraint("name", "measured_time"),)
+    children = relationship("RegularLt", uselist=False,
+                            back_populates="parent")
+
+    children2 = relationship("GasexLt", uselist=False,
+                            back_populates="parent")
+
+
+class RegularLt(Base):
+    __tablename__ = 'regular_lt'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    ltid = Column(Integer, ForeignKey('lt.id'))
+    surfactant = Column(Text)
+    sensitizer = Column(Text)
+    ratio = Column(Text)
+    conc = Column(Text)
+    spreading_volume = Column(Text)
+    speed = Column(Text)
+    sample_no = Column(Text)
+    measurement_no = Column(Text)
+    parent = relationship("Lt", uselist=False, back_populates="children")
+
+
+class GasexLt(Base):
+    __tablename__ = 'gasex_lt'
+    id = Column(Integer, primary_key=True)
+    ltid = Column(Integer, ForeignKey('lt.id'))
+    name = Column(Text)
+    sample_id = Column(Integer, ForeignKey('samples.id'))
+    sample_hash = Column(Text)
+    parent = relationship("Lt",  uselist=False, back_populates="children2")
+
+
+class GasexSurftens(Base):
+    __tablename__ = 'gasex_surftens'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    surface_tension = Column(Text)
+    sample_id = Column(Integer, ForeignKey('samples.id'))
+
+
+class Substances(Base):
+    __tablename__ = 'substances'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, unique=True)
+    abbreviation = Column(Text)
+    molar_mass = Column(Text)
+    sensitizing = Column(Text)
+
+
+class Stations(Base):
+    __tablename__ = 'stations'
+    id = Column(Integer, primary_key=True)
+    hash = Column(Text, unique=True)
+    type = Column(Text)
+    date = Column(TIMESTAMP)
+    number = Column(Integer)
+    label = Column(Text)
+    longitude = Column(Text)
+    latitude = Column(Text)
+    surface_salinity = Column(Text)
+    deep_salinity = Column(Text)
+    surface_temperature = Column(Text)
+    deep_temperature = Column(Text)
+
+
+class StationStat(Base):
+    __tablename__ = 'station_stats'
+    id = Column(Integer, primary_key=True)
+    station_id = Column(Integer, ForeignKey("stations.id"), unique=True)
+    plate_coverage = Column(Float)
+    plate_coverage_std = Column(Float)
+    plate_coverage_n = Column(Integer)
+
+    plate_lift_off = Column(Float)
+    plate_lift_off_std = Column(Float)
+    plate_lift_off_n = Column(Integer)
+
+    plate_tension = Column(Float)
+    plate_tension_std = Column(Float)
+    plate_tension_n = Column(Integer)
+
+    plate_max_pressure = Column(Float)
+    plate_max_pressure_std = Column(Float)
+    plate_max_pressure_n = Column(Integer)
+
+    screen_coverage = Column(Float)
+    screen_coverage_std = Column(Float)
+    screen_coverage_n = Column(Integer)
+
+    screen_lift_off = Column(Float)
+    screen_lift_off_std = Column(Float)
+    screen_lift_off_n = Column(Integer)
+
+    screen_tension = Column(Float)
+    screen_tension_std = Column(Float)
+    screen_tension_n = Column(Integer)
+
+    screen_max_pressure = Column(Float)
+    screen_max_pressure_std = Column(Float)
+    screen_max_pressure_n = Column(Integer)
+
+    sml_coverage = Column(Float)
+    sml_coverage_std = Column(Float)
+    sml_coverage_n = Column(Integer)
+
+    sml_lift_off = Column(Float)
+    sml_lift_off_std = Column(Float)
+    sml_lift_off_n = Column(Integer)
+
+    sml_tension = Column(Float)
+    sml_tension_std = Column(Float)
+    sml_tension_n = Column(Integer)
+
+    sml_max_pressure = Column(Float)
+    sml_max_pressure_std = Column(Float)
+    sml_max_pressure_n = Column(Integer)
+
+    deep_coverage = Column(Float)
+    deep_coverage_std = Column(Float)
+    deep_coverage_n = Column(Integer)
+
+    deep_lift_off = Column(Float)
+    deep_lift_off_std = Column(Float)
+    deep_lift_off_n = Column(Integer)
+
+    deep_tension = Column(Float)
+    deep_tension_std = Column(Float)
+    deep_tension_n = Column(Integer)
+
+    deep_max_pressure = Column(Float)
+    deep_max_pressure_std = Column(Float)
+    deep_max_pressure_n = Column(Integer)
+
+    sml_rawtension = Column(Float)
+    deep_rawtension = Column(Float)
+
+
+class Samples(Base):
+    __tablename__ = 'samples'
+    id = Column(Integer, primary_key=True)
+    station_id = Column(Integer, ForeignKey('stations.id'))
+    sample_hash = Column(Text, unique=True)
+    location = Column(Text)
+    type = Column(Text)
+    number = Column(Integer)
+    coverage = Column(Float)
+    max_pressure = Column(Float)
+    lift_off = Column(Float)
+    surface_tension = Column(Float)
+
+
+class IR(Base):
+    __tablename__ = 'ir'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    wavenumbers = Column(Text)
+    transmission = Column(Text)
+
+
+class Raman(Base):
+    __tablename__ = 'raman'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    wavenumbers = Column(Text)
+    intensity = Column(Text)
+
+
+class UV(Base):
+    __tablename__ = 'uv'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    wavelength = Column(Text)
+    absorbance = Column(Text)
+
 
 class DatabaseWizard:
-
-    Sfg = {
-        "__tablename__": 'sfg',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "measured_time": Column(TIMESTAMP),
-        "measurer": Column(Text),
-        "type": Column(Text),
-        "wavenumbers": Column(Text),
-        "sfg": Column(Text),
-        "ir": Column(Text),
-        "vis": Column(Text),
-    }
-
-    BoknisEck = {
-        "__tablename__": 'boknis_eck',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text, unique=True),
-        "specid": Column(Integer, ForeignKey('sfg.id'))
-    }
-
-    GasExSfg = {
-        "__tablename__": 'gasex_sfg',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text, ForeignKey('sfg.name'), unique=True),
-        "sample_id": Column(Integer, ForeignKey('samples.id')),
-        "sample_hash": Column(Text)
-    }
-
-    RegularSfg = {
-        "__tablename__": 'regular_sfg',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "specid": Column(Integer, ForeignKey('sfg.id')),
-        "surfactant": Column(Text),
-        "surfactant_vol": Column(Text),
-        "surfactant_conc": Column(Text),
-        "sensitizer": Column(Text),
-        "sensitizer_vol": Column(Text),
-        "sensitizer_conc": Column(Text),
-        "photolysis": Column(Text),
-        "sample_no": Column(Text),
-        "measurement_no": Column(Text),
-        "comment": Column(Text)
-    }
-
-    Lt = {
-        "__tablename__": 'lt',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "type": Column(Text),
-        "measured_time": Column(TIMESTAMP),
-        "measurer": Column(Text),
-        "time": Column(Text),
-        "area": Column(Text),
-        "apm": Column(Text),
-        "surface_pressure": Column(Text),
-        "lift_off": Column(Text),
-    }
-
-    RegularLt = {
-        "__tablename__": 'regular_lt',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "ltid": Column(Integer, ForeignKey('lt.id')),
-        "surfactant": Column(Text),
-        "sensitizer": Column(Text),
-        "ratio": Column(Text),
-        "conc": Column(Text),
-        "spreading_volume": Column(Text),
-        "speed": Column(Text),
-        "sample_no": Column(Text),
-        "measurement_no": Column(Text),
-    }
-
-    GasexLt = {
-        "__tablename__": 'gasex_lt',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "sample_id": Column(Integer, ForeignKey('samples.id')),
-        "sample_hash": Column(Text, ForeignKey('gasex_lt.name')),
-    }
-
-    GasexSurftens = {
-        "__tablename__": 'gasex_surftens',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "surface_tension": Column(Text),
-        "sample_id": Column(Integer, ForeignKey('samples.id')),
-    }
-
-    Substances = {
-        "__tablename__": 'substances',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text, unique=True),
-        "abbreviation": Column(Text),
-        "molar_mass": Column(Text),
-        "sensitizing": Column(Text),
-    }
-
-    Stations = {
-        "__tablename__": 'stations',
-        "id": Column(Integer, primary_key=True),
-        "hash": Column(Text, unique=True),
-        "type": Column(Text),
-        "date": Column(TIMESTAMP),
-        "number": Column(Integer),
-        "label": Column(Text),
-        "longitude": Column(Text),
-        "latitude": Column(Text),
-        "surface_salinity": Column(Text),
-        "deep_salinity": Column(Text),
-        "surface_temperature": Column(Text),
-        "deep_temperature": Column(Text),
-    }
-
-    StationStat = {
-
-        "__tablename__": 'station_stats',
-        "id": Column(Integer, primary_key=True),
-        "station_id": Column(Integer, ForeignKey("stations.id"), unique=True),
-        "plate_coverage": Column(Float),
-        "plate_coverage_std": Column(Float),
-        "plate_coverage_n": Column(Integer),
-
-        "plate_lift_off": Column(Float),
-        "plate_lift_off_std": Column(Float),
-        "plate_lift_off_n": Column(Integer),
-
-        "plate_tension": Column(Float),
-        "plate_tension_std": Column(Float),
-        "plate_tension_n": Column(Integer),
-
-        "plate_max_pressure": Column(Float),
-        "plate_max_pressure_std": Column(Float),
-        "plate_max_pressure_n": Column(Integer),
-
-        "screen_coverage": Column(Float),
-        "screen_coverage_std": Column(Float),
-        "screen_coverage_n": Column(Integer),
-
-        "screen_lift_off": Column(Float),
-        "screen_lift_off_std": Column(Float),
-        "screen_lift_off_n": Column(Integer),
-
-        "screen_tension": Column(Float),
-        "screen_tension_std": Column(Float),
-        "screen_tension_n": Column(Integer),
-
-        "screen_max_pressure": Column(Float),
-        "screen_max_pressure_std": Column(Float),
-        "screen_max_pressure_n": Column(Integer),
-
-        "sml_coverage": Column(Float),
-        "sml_coverage_std": Column(Float),
-        "sml_coverage_n": Column(Integer),
-
-        "sml_lift_off": Column(Float),
-        "sml_lift_off_std": Column(Float),
-        "sml_lift_off_n": Column(Integer),
-
-        "sml_tension": Column(Float),
-        "sml_tension_std": Column(Float),
-        "sml_tension_n": Column(Integer),
-
-        "sml_max_pressure": Column(Float),
-        "sml_max_pressure_std": Column(Float),
-        "sml_max_pressure_n": Column(Integer),
-
-        "deep_coverage": Column(Float),
-        "deep_coverage_std": Column(Float),
-        "deep_coverage_n": Column(Integer),
-
-        "deep_lift_off": Column(Float),
-        "deep_lift_off_std": Column(Float),
-        "deep_lift_off_n": Column(Integer),
-
-        "deep_tension": Column(Float),
-        "deep_tension_std": Column(Float),
-        "deep_tension_n": Column(Integer),
-
-        "deep_max_pressure": Column(Float),
-        "deep_max_pressure_std": Column(Float),
-        "deep_max_pressure_n": Column(Integer),
-
-        "sml_rawtension":  Column(Float),
-        "deep_rawtension": Column(Float),
-    }
-
-    Samples = {
-        "__tablename__": 'samples',
-        "id": Column(Integer, primary_key=True),
-        "station_id": Column(Integer, ForeignKey('stations.id')),
-        "sample_hash": Column(Text, unique=True),
-        "location": Column(Text),
-        "type": Column(Text),
-        "number": Column(Integer),
-        "coverage": Column(Float),
-        "max_pressure": Column(Float),
-        "lift_off": Column(Float),
-        "surface_tension": Column(Float),
-    }
-
-    IR = {
-        "__tablename__": 'ir',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "wavenumbers": Column(Text),
-        "transmission": Column(Text),
-    }
-
-    Raman = {
-        "__tablename__": 'raman',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "wavenumbers": Column(Text),
-        "intensity": Column(Text),
-    }
-
-    uv = {
-        "__tablename__": 'uv',
-        "id": Column(Integer, primary_key=True),
-        "name": Column(Text),
-        "wavelength": Column(Text),
-        "absorbance": Column(Text),
-    }
 
     def __init__(self):
 
         # SQL init
         self.engine = create_engine('sqlite:///orm.db', echo=False)
-        self.Base = declarative_base()
+        self.Base = Base
 
         # ORM object creation
-        self.sfg = self.factory(DatabaseWizard.Sfg)
-        self.regular_sfg = self.factory(DatabaseWizard.RegularSfg)
+        self.sfg = SFG
+        self.regular_sfg = RegularSfg
 
-        self.lt = self.factory(DatabaseWizard.Lt)
-        self.regular_lt = self.factory(DatabaseWizard.RegularLt)
+        self.lt = Lt
+        self.regular_lt = RegularLt
 
-        self.boknis_eck = self.factory(DatabaseWizard.BoknisEck)
+        self.boknis_eck = BoknisEck
 
-        self.substances = self.factory(DatabaseWizard.Substances)
+        self.substances = Substances
 
-        self.stations = self.factory(DatabaseWizard.Stations)
-        self.station_stats = self.factory(DatabaseWizard.StationStat)
-        self.samples = self.factory(DatabaseWizard.Samples)
-        self.gasex_surftens = self.factory(DatabaseWizard.GasexSurftens)
-        self.gasex_lt = self.factory(DatabaseWizard.GasexLt)
-        self.gasex_sfg = self.factory(DatabaseWizard.GasExSfg)
+        self.stations = Stations
+        self.station_stats = StationStat
+        self.samples = Samples
+        self.gasex_surftens = GasexSurftens
+        self.gasex_lt = GasexLt
+        self.gasex_sfg = GasExSfg
 
-        self.ir = self.factory(DatabaseWizard.IR)
-        self.raman = self.factory(DatabaseWizard.Raman)
-        self.uv = self.factory(DatabaseWizard.uv)
+        self.ir = IR
+        self.raman = Raman
+        self.UV = UV
 
         # SQL creation
         self.Base.metadata.create_all(self.engine)
@@ -291,7 +305,6 @@ class DatabaseWizard:
 
         elif name == "lt":
             dic["__table_args__"] = ((UniqueConstraint("name", "measured_time")),)
-
         return type(name, (self.Base,), dic)
 
 
@@ -420,7 +433,7 @@ class ImportDatabaseWizard(DatabaseWizard):
         self.session.add_all(ir)
 
         for item in self.importer.uv:
-            spec = self.uv()
+            spec = self.UV()
             spec.name = item["name"]
             spec.wavelength = ",".join(item["data"]["x"].values.astype(str))
             spec.absorbance = ",".join(item["data"]["y"].values.astype(str))
