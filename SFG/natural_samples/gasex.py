d04b00444ebcc107b4ea96bd0b81c343f9096175
Darkskald <32447347+Darkskald@users.noreply.github.com>
2020-03-02 13:05:25 +0100

    adds a new class (GasExWorkDatabaseWizard) to handle GasEx analysis

--- SFG/natural_samples/gasex.py
+++ SFG/natural_samples/gasex.py
@@ -2,6 +2,8 @@ from SFG.orm.orm import WorkDatabaseWizard
 
 import numpy as np
 import pandas as pd
+from scipy import stats
+from typing import Tuple
 
 
 class GasExManager:
@@ -84,7 +86,6 @@ class GasExManager:
 
             station.get_all_stats()
             station.reference_tension_temperature()
-
 
     def persist_stations(self):
         """Invokes the persist_stat function of each station, ensuring that the calculated values are written to
@@ -284,6 +285,97 @@ class Station:
         factor = 1 + (3.766 * 10 ** -4) * s + (2.347 * 10 ** -6) * s * t
         return temp * factor
 
+
+class GasExWorkDatabaseWizard(WorkDatabaseWizard):
+
+    def __init__(self):
+        super().__init__()
+
+    def load_station_data(self) -> pd.DataFrame:
+        """Extracts all station data as Pandas dataframe"""
+
+        cmd = """
+            SELECT 
+            
+            date AS 'sampling date',
+            doy AS 'day of the year',
+            
+            plate_coverage AS 'surface coverage plate',
+            plate_coverage_std,
+            plate_tension AS 'surface tension plate',
+            plate_tension_std,
+            plate_lift_off AS 'lift-off compression ratio plate',
+            plate_lift_off_std,
+            plate_max_pressure AS 'max. surface pressure plate',
+            plate_max_pressure_std, 
+            
+            screen_coverage AS 'surface coverage screen',
+            screen_coverage_std,
+            screen_tension AS 'surface tension screen',
+            screen_tension_std,
+            screen_lift_off AS 'lift-off compression ratio screen',
+            screen_lift_off_std,
+            screen_max_pressure AS 'max. surface pressure screen',
+            screen_max_pressure_std, 
+            
+            sml_coverage AS 'surface coverage SML',
+            sml_coverage_std,
+            sml_tension AS 'surface tension SML',
+            sml_tension_std,
+            sml_lift_off AS 'lift-off compression ratio SML',
+            sml_lift_off_std,
+            sml_max_pressure AS 'max. surface pressure SML',
+            sml_max_pressure_std,
+            
+            
+            deep_coverage AS 'surface coverage bulk',
+            deep_coverage_std,
+            deep_tension AS 'surface tension bulk',
+            deep_tension_std,
+            deep_lift_off AS 'lift-off compression ratio bulk',
+            deep_lift_off_std,
+            deep_max_pressure AS 'max. surface pressure bulk',
+            deep_max_pressure_std 
+            
+            FROM stations
+            INNER JOIN station_stats
+            on stations.id = station_stats.station_id;
+            """
+        return GasExWorkDatabaseWizard.yield_df_from_sql(cmd, self.engine)
+
+    def load_samples_by_type(self, _type) -> pd.DataFrame:
+
+        base_cmd = """SELECT
+                    sample_hash AS 'name', 
+                    coverage AS 'surface coverage',
+                    max_pressure AS 'max. surface pressure',
+                    lift_off AS 'lift-off compression ratio',
+                    surface_tension AS 'surface tension'
+                    FROM samples
+                    """
+        end_cmd = {
+            "all": ";",
+            "sml": """WHERE type='p' OR type='s';""",
+            "bulk": """WHERE type='deep';""",
+            "p": """WHERE type='p';""",
+            "s": """WHERE type='s';""",
+            "alkor": """WHERE location='a';""",
+            "rubber": """WHERE location='r';""",
+        }
+        cmd = f'{base_cmd}{end_cmd[_type]}'
+        return GasExWorkDatabaseWizard.yield_df_from_sql(cmd, self.engine)
+
+    @staticmethod
+    def yield_df_from_sql(sql_statement: str, db) -> pd.DataFrame:
+        """Auxiliary function to load data from an SQL database yielding a dataframe"""
+        return pd.read_sql(sql_statement, db)
+
+    @staticmethod
+    def apply_test(df: pd.DataFrame, column: str, test_func) -> Tuple[float, float]:
+        """Function to apply a statistical test function to a column of a dataframe"""
+        temp = df[column].to_numpy()
+        temp = temp[np.logical_not(np.isnan(temp))]
+        return test_func(temp)
 
 if __name__ == "__main__":
     GasExManager()
