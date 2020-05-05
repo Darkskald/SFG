c126167040a43b76af78cd1ba2680beb0b2cd4be
flo <floriandavid.lannge@web.de>
2020-05-05 23:33:12 +0200

    - multiprocessing support for sfg, clean-up

--- SFG/orm/importer.py
+++ SFG/orm/importer.py
@@ -17,7 +17,7 @@ class Importer:
     def __init__(self):
         # paths
         self.base_path = Path.cwd() / 'newport'
-        # todo dieser Pfadspaß ist miserabel
+        # todo dieser Pfadspaß ist miserabel, das sollte durch ein yaml oder json geladen werden
         self.paths = {
             "regular_sfg": self.base_path / 'regular',
             "gasex_sfg": self.base_path / 'gasex_sfg',
@@ -31,33 +31,33 @@ class Importer:
             "uv": self.base_path / 'UV',
             "raman": self.base_path / 'Raman',
             "station_plan": self.base_path / 'stationsplan.xls',
-            "chlorophyll": self.base_path / 'be_data.csv',
+            "be_database_parameters": self.base_path / 'be_data.csv',
             "water_samples": self.base_path / 'Wasserproben_komplett.xlsx'
         }
-
         # sfgs
-        self.regular_sfg = self.import_sfg(self.paths["regular_sfg"])
-        self.gasex_sfg = self.import_sfg(self.paths["gasex_sfg"])
-        self.boknis = self.import_sfg(self.paths["boknis"])
+        p = Pool(3)
+        data = p.map(self.import_sfg, [self.paths["regular_sfg"], self.paths["gasex_sfg"], self.paths["boknis"]])
+        p.close()
+        self.regular_sfg, self.gasex_sfg, self.boknis = data
 
         # lts
-        # self.lt = self.import_lt(self.paths["lt"])
-        # self.gasex_lt = self.import_lt(self.paths["gasex_lt"])
-        # self.gasex_lift_off = self.import_liftoffs(self.paths["gasex_lift_off"])
+        self.lt = self.import_lt(self.paths["lt"])
+        self.gasex_lt = self.import_lt(self.paths["gasex_lt"])
+        self.gasex_lift_off = self.import_liftoffs(self.paths["gasex_lift_off"])
 
         # spectra
-        # self.ir = []
-        # self.uv = []
-        # self.raman = []
-        # self.import_other_spectra()
+        self.ir = self.import_xy_spectra("ir")
+        self.uv = self.import_xy_spectra("uv")
+        self.raman = self.import_xy_spectra("raman")
 
-        # surface tension and salinity
-        # todo: chlorophyll etc
-        # self.gasex_tension = self.import_tensions(self.paths["gasex_tension"])
-        # self.salinity = self.import_salinity()
+        # surface tension, GasEx cruise data and Boknis Eck parameters
+        self.gasex_tension = pd.read_csv(self.paths["gasex_tension"], sep=';', names=["name", "tension"])
+        self.station_plan = pd.read_excel(self.paths["station_plan"])
+        self.be_database_parameters = pd.read_csv(self.paths["be_database_parameters"], sep=",", header=0, engine='c')
+        self.water_samples = pd.read_excel(self.paths["water_samples"])
 
         # substances
-        # self.substances = self.import_substances(self.paths["substances"])
+        self.substances = self.import_substances(self.paths["substances"])
 
     # SFG
     def import_sfg(self, parent_dir):
@@ -68,27 +68,30 @@ class Importer:
 
         for directory in parent_dir.iterdir():
             files = directory.rglob('*.sfg')
-            data = list(map(partial(self.aux, func=self.extract_sfg_file), files))
+            data = list(map(partial(self.aux, func=self.extract_sfg_file, sfg=True), files))
             out.extend(data)
         return out
 
-    def aux(self, file, func):
+    def aux(self, file, func, sfg):
 
         creation_time = datetime.datetime.fromtimestamp(file.stat().st_ctime)
         data = func(file)
         dic = {"name": file.name, "type": file.parent.parent, "measured_time": creation_time, "data": data}
 
-        if "dppc" in dic["name"] or "DPPC" in dic["name"]:
-            if "boknis" in file.parent.parent.name:
-                dic["type"] = "boknis_ref"
-            else:
-                dic["type"] = "regular"
+        if sfg:
+            if "dppc" in dic["name"] or "DPPC" in dic["name"]:
+                if "boknis" in file.parent.parent.name:
+                    dic["type"] = "boknis_ref"
+                else:
+                    dic["type"] = "regular"
         return dic
 
-    def extract_sfg_file(self, file):
+    @staticmethod
+    def extract_sfg_file(file):
         """A function extracting the measurement data from a SFG spectral file """
         col_names = ['wavenumbers', 'sfg', 'ir', 'vis']
-        temp = pd.read_csv(file, sep="\t", usecols=[0, 1, 3, 4], names=col_names, encoding='utf8', engine='python')
+        # todo: changed this to c engine for reasons of performance
+        temp = pd.read_csv(file, sep="\t", usecols=[0, 1, 3, 4], names=col_names, encoding='utf8', engine='c')
         return temp
 
     # LT
@@ -98,107 +101,49 @@ class Importer:
 
         out = []
 
-        for parent_dir in os.listdir(head_dir):
-            new_path = head_dir + "/" + parent_dir
-            if os.path.isdir(new_path):
+        for parent_dir in head_dir.iterdir():
 
-                for file in os.listdir(new_path):
-
-                    if file.endswith(".dat"):
-                        creation_time = datetime.datetime.fromtimestamp(os.path.getmtime(new_path + "/" + file))
-                        name = file[:-4]
-
-                        data = self.extract_lt_file(new_path + "/" + file)
-                        dic = {"name": name, "type": head_dir.split("/")[-1], "measured_time": creation_time,
-                               "data": data}
-
-                        out.append(dic)
+            if parent_dir.is_dir():
+                files = parent_dir.rglob('*.dat')
+                data = list(map(partial(self.aux, func=self.extract_lt_file, sfg=False), files))
+                out.extend(data)
         return out
 
-    def extract_lt_file(self, file):
+    @staticmethod
+    def extract_lt_file(file):
         """A function extracting the measurement data from a SFG spectral file """
         col_names = ["time", "area", "apm", "surface_pressure"]
-        temp = pd.read_csv(file, comment="#", sep='\t', usecols=[1, 2, 3, 4], names=col_names)
+        temp = pd.read_csv(file, comment="#", sep='\t', usecols=[1, 2, 3, 4], names=col_names, engine="c")
         return temp
 
-    def import_liftoffs(self, file):
+    @staticmethod
+    def import_liftoffs(file):
         """Imports the lift-off points determined manually from a file"""
         col_names = ["name", "lift_off"]
         temp = pd.read_csv(file, sep=';', names=col_names, skiprows=1)
         return temp
 
     # other spectra
-    def extract_other_spectra(self, file, sep='\t', skip=0):
+    @staticmethod
+    def extract_xy_spectra(file, sep='\t', skip=0):
         """Extracts the data from UV, IR and Raman measurements"""
         temp = pd.read_csv(file, sep=sep, skiprows=skip, names=["x", "y"])
-        return temp
+        return {"name": file.name, "data": temp}
+
+    def import_xy_spectra(self, spec_type):
+        if spec_type != "uv":
+            data = map(self.extract_xy_spectra, self.paths[spec_type].iterdir())
+        else:
+            data = map(partial(self.extract_xy_spectra, skip=2, sep=","), self.paths[spec_type].iterdir())
+        return data
 
     # other
-    def import_substances(self, file):
+    @staticmethod
+    def import_substances(file):
         """Extracts the information about predefined substances out of the corresponding file"""
         with open(file) as infile:
             return json.load(infile)
 
-    def import_tensions(self, file):
-        col_names = ["name", "tension"]
-        temp = pd.read_csv(file, sep=';', names=col_names)
-        return temp
-
-    def import_other_spectra(self):
-
-        # todo: this function is not documented and looks unprofessional
-
-        for file in os.listdir(str(self.paths["raman"])):
-            temp_df = self.extract_other_spectra(str(self.paths["raman"] / file))
-            dic = {"name": file, "data": temp_df}
-            self.raman.append(dic)
-
-        for file in os.listdir(str(self.paths["ir"])):
-            temp_df = self.extract_other_spectra(str(self.paths["ir"] / file))
-            dic = {"name": file, "data": temp_df}
-            self.ir.append(dic)
-
-        for file in os.listdir(str(self.paths["uv"])):
-            temp_df = self.extract_other_spectra(str(self.paths["uv"] / file), skip=2, sep=",")
-            dic = {"name": file, "data": temp_df}
-            self.uv.append(dic)
-
-    def import_salinity(self):
-        # todo: load this directly into the database without salinity selection
-        sal = pd.read_excel(self.paths["station_plan"])
-        salinities = []
-        for row in range(len(sal)):
-            sur_sal = sal.loc[row, "Salinity surface"]
-            dep_sal = sal.loc[row, "Salinity depth"]
-            sur_temp = sal.loc[row, "Temperature surface"]
-            dep_temp = sal.loc[row, "Temperature depth"]
-            _hash = '0' + str(sal.loc[row, "hash"])
-            label = sal.loc[row, "Leg"] + "-" + str(sal.loc[row, "Station Number"])
-            lat = sal.loc[row, "Latitude"]
-            long = sal.loc[row, "Longitude"]
-
-            dic = {"surface_salinity": sur_sal, "deep_salinity": dep_sal,
-                   "label": label, "longitude": long, "latitude": lat, "hash": _hash,
-                   "surface_temperature": sur_temp, "deep_temperature": dep_temp}
-            salinities.append(dic)
-        return salinities
-
-    # auxiliary functions
-
-
-"""
-@staticmethod
-    def prepare_chorophyll_data():
-        print(f'{os.getcwd()} IN BE')
-        be = pd.read_csv(os.getcwd() + "/newport/be_data.csv", sep=",", header=0)
-        be = be[be["Depth [m]"] == 1]
-        be["Time"] = pd.to_datetime(be["Time"])
-        be["Time"] = be["Time"].apply(lambda x: x.date())
-        return be
-"""
-
-# todo: decouple the Importer so it is not to tightly bound to boknis/gasex
-# todo: implement async io
 
 start = timeit.default_timer()
 i = Importer()
