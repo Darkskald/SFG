2743400d0e00a2b8594c88863e7d4847d20e0bf4
darkskald <floriandavid.lange@web.de>
2020-08-02 19:55:11 +0200

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SFG',
+    version='0.5',
+    packages=['SFG', 'SFG.orm', 'SFG.legacy', 'SFG.spectrum', 'SFG.natural_samples'],
+    url='https://github.com/Darkskald/SFG',
+    license='MIT',
+    author='Darkskald',
+    author_email='',
+    description=''
+)
