a85f387839a0da92c591319932e96bd06d704185
darkskald <floriandavid.lange@web.de>
2020-08-06 22:53:52 +0200

    - cruise field for sample dictionary

--- SFG/scratch.py
+++ SFG/scratch.py
@@ -5,8 +5,6 @@ from SFG.orm.interact import DbInteractor, SampleProcessor
 interactor = DbInteractor()
 samples = interactor.session.query(interactor.samples).all()
 
-test = SampleProcessor(samples, interactor)
-temp = test.get_list_of_sample_dicts()
-temp = DataFrame(temp)
-
-print(temp[notnull(temp["lift_off_compression_ration"])])
+test = SampleProcessor(samples, interactor).get_list_of_sample_dicts()
+df = DataFrame(test)
+print(df[["sample_hash", "cruise"]])
