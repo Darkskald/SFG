f214ecd1bbe2355001136c1252d977dbc945fdb2
darkskald <floriandavid.lange@web.de>
2020-08-06 21:29:22 +0200

    - adds sample to dictionary conversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SFG',
-    version='0.5',
+    version='0.51',
     packages=['SFG', 'SFG.orm', 'SFG.legacy', 'SFG.spectrum', 'SFG.natural_samples'],
     url='https://github.com/Darkskald/SFG',
     license='MIT',
