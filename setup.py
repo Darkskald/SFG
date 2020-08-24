f3679648ae3e05b3e0134c6c2936c4ebe8523093
darkskald <floriandavid.lange@web.de>
2020-08-24 19:29:05 +0200

    - removed dangerous deprecation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SFG',
-    version='0.6.3.4',
+    version='0.6.3.5',
     packages=['SFG', 'SFG.orm', 'SFG.legacy', 'SFG.spectrum', 'SFG.natural_samples'],
     url='https://github.com/Darkskald/SFG',
     license='MIT',
