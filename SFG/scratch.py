1404f11c5fcddc3f33928c03b115ec3ac6143e9c
darkskald <floriandavid.lange@web.de>
2020-08-27 00:13:17 +0200

    update and improve Plot.ly interface

--- SFG/scratch.py
+++ SFG/scratch.py
@@ -7,6 +7,9 @@ from SFG.orm.interact import DbInteractor
 
 import pandas as pd
 
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
 interactor = DbInteractor()
 stations = interactor.session.query(interactor.stations).all()
 samples = interactor.session.query(interactor.samples).all()
@@ -14,9 +17,32 @@ samp = SampleProcessor(samples, interactor)
 smps = pd.DataFrame(samp.get_list_of_sample_dicts())
 
 sps = SamplePlotProcessor(smps)
-out = sps.unwrap_properties_for_plotting(*sps.split_dataset("type", ('s', 'p')))
+sps.df["type"] = sps.gather_sml()
+out = sps.unwrap_properties_for_plotting(*sps.split_dataset("type", ('sml', 'deep')))
 
-print(out)
+
+
+def plot_sample_by_plotly(dic):
+    fig = make_subplots(rows=2, cols=2)
+    coord = [(1, 1), (1, 2), (2, 1), (2, 2)]
+    count = 0
+
+    for key in dic:
+        row, col = coord[count]
+        if count == 0:
+            fig.add_trace(go.Box(**dic[key][0], marker_color="red", boxmean=True), row=row, col=col)
+            fig.add_trace(go.Box(**dic[key][1], marker_color="green", boxmean=True), row=row, col=col)
+        else:
+            fig.add_trace(go.Box(**dic[key][0], marker_color="red", boxmean=True, showlegend=False), row=row, col=col)
+            fig.add_trace(go.Box(**dic[key][1], marker_color="green", boxmean=True, showlegend=False), row=row, col=col)
+        fig.add_trace(go.Box(**dic[key][2], marker_color="red", boxmean=True, showlegend=False), row=row, col=col)
+        fig.add_trace(go.Box(**dic[key][3], marker_color="green", boxmean=True, showlegend=False), row=row, col=col)
+        fig.update_yaxes(title_text=key, row=row, col=col)
+        count += 1
+
+    fig.update_layout(height=1000, width=1400, font_size=18)
+    fig.show()
+plot_sample_by_plotly(out)
 """
 stations = interactor.session.query(interactor.stations).all()
 samples = interactor.session.query(interactor.samples).all()
@@ -53,4 +79,3 @@ df = sp.get_station_data_frame()
 
 
 """
-
